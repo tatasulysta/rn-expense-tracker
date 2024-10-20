36ec1f162e236f7a1a9d0d67c436d276b8637d86
Tata Sulysta <tatasulystaaa@gmail.com>
2024-10-20 23:11:04 +0700

    remove unused file

--- src/components/common/text-info.tsx
+++ src/components/common/text-info.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyledText, StyledView } from "../common";
 import { InfoIcon } from "../../assets";
-import { tw } from "nativewind";
 
 interface Props extends React.PropsWithChildren {
   withIndent?: boolean;
@@ -12,9 +11,6 @@ export default function TextInfo(props: Props) {
       className={`flex flex-1 items-center flex-row gap-2 mt-1 ${
         props.withIndent && "mx-2"
       }`}
-      style={{
-        ...useTailwind,
-      }}
     >
       <InfoIcon size={16} />
       <StyledText className="text-sm text-neutral-500">
